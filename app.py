d5eed948fd1c4547182c821070f51682690b4794
Teemu I <teemu.itapelto@gmail.com>
2021-01-25 14:00:50 +0200

    added function to add users to database

--- app.py
+++ app.py
@@ -1,1 +1,32 @@
-g
+from flask import Flask, render_template, request
+from flask_mysqldb import MySQL
+app = Flask(__name__)
+
+
+# Connecting to db
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_DB'] = 'demo'
+
+mysql = MySQL(app)
+
+
+# Adding data to db, table Users
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == "POST":
+        details = request.form
+        firstName = details['fname']
+        lastName = details['lname']
+        cur = mysql.connection.cursor()
+        cur.execute(
+            "INSERT INTO Users(firstName, lastName) VALUES (%s, %s)", (firstName, lastName))
+        mysql.connection.commit()
+        cur.close()
+
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
